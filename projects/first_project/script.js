ab99621547165a7b5d4132ca14e736ac5ae3d3ad
anandbalmiki <anandbalmiki285@gmail.com>
2021-06-09 10:20:18 +0530

    Third file updated

--- projects/first_project/script.js
+++ projects/first_project/script.js
@@ -1,5 +1,5 @@
 
-const panels = document.querySelectorAll('.panel')
+/*const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
@@ -13,3 +13,35 @@ function removeActiveClasses() {
         panels  .classList.remove('active')
     })
 } 
+*/
+
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    
+    panel.addEventListener('click', function() {
+        removee();
+        panel.classList.add('active');
+    })
+})
+
+function removee() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+        console.log('yo');
+    })
+}
+
+var flag = 0;
+
+document.querySelector('body').addEventListener('keypress', function() {
+    if(flag===0) {
+        document.querySelector('body').style.backgroundColor = 'white';
+        flag = 1;
+        
+    } else {
+        document.querySelector('body').style.backgroundColor = 'black';
+        flag = 0;
+        
+    }
+})
